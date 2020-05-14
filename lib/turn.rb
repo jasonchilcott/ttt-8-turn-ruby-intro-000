b9238ab1074f69b3e7795bf6da638bb690b9ce77
Jason Chilcott <jasonchilcott@me.com>
2020-05-14 21:15:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,25 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def position_taken?(board,index)
+  if 
+    (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else  
+    return true
+  end  
+end    
+
+def valid_move?(board, index)
+  if 
+    position_taken?(board, index)
+    return false
+  elsif 
+    board[index].between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
+      
